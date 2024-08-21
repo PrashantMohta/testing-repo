2cbbcdb95715f7c0c6566331ae3e900b3e305445
Prashant Mohta <mohtaprashantrocks@gmail.com>
2024-08-22 02:02:44 +0700

    update

--- .github/workflows/update-modlinks.py
+++ .github/workflows/update-modlinks.py
@@ -17,7 +17,7 @@ else:
     new_version = sys.argv[2]
     new_link = sys.argv[3]
     new_sha = sys.argv[4]
-
+print({'name':name,'version':new_version,'link':new_link , 'sha': new_sha})
 tag_name = "{https://github.com/HollowKnight-Modding/HollowKnight.ModLinks/HollowKnight.ModManager}Name"
 tag_version = "{https://github.com/HollowKnight-Modding/HollowKnight.ModLinks/HollowKnight.ModManager}Version"
 tag_link = "{https://github.com/HollowKnight-Modding/HollowKnight.ModLinks/HollowKnight.ModManager}Link"
@@ -27,7 +27,7 @@ old_modlinks_data = {'name':'','version':'','link':'' , 'sha': ''}
 tree = ET.parse('ModLinks.xml')
 root = tree.getroot()
 for child in root:
-    if child.find(tag_name).text == "Custom Knight" :
+    if child.find(tag_name).text == name :
         old_modlinks_data['name'] = child.find(tag_name).text.strip()
         old_modlinks_data['version'] = child.find(tag_version).text.strip()
         old_modlinks_data['link'] = child.find(tag_link).text.strip()
@@ -38,9 +38,14 @@ print(old_modlinks_data)
 
 modlink_str = open('ModLinks.xml','r').read()
 
-modlink_str = modlink_str.replace(old_modlinks_data['version'],new_version)
-modlink_str = modlink_str.replace(old_modlinks_data['link'],new_link)
-modlink_str = modlink_str.replace(old_modlinks_data['sha'],new_sha)
+if len(old_modlinks_data['version']) > 0:
+    modlink_str = modlink_str.replace(old_modlinks_data['version'],new_version)
+
+if len(old_modlinks_data['link']) > 0:
+    modlink_str = modlink_str.replace(old_modlinks_data['link'],new_link)
+
+if len(old_modlinks_data['sha']) > 0:
+    modlink_str = modlink_str.replace(old_modlinks_data['sha'],new_sha)
 
 with open("ModLinks.xml", "w") as f:
     f.write(modlink_str)
